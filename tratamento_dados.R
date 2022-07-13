6c68ea55b7cab717ec497d7303ff2d3dffa31add
thiago fofano <tfofano3@gmail.com>
2022-07-13 19:12:03 -0300

    atualização 13/07/2022

--- tratamento_dados.R
+++ tratamento_dados.R
@@ -15,29 +15,23 @@ getwd()
 dados <- read.csv("dados_covid_sp.csv", sep = ";",na.strings = "" ,stringsAsFactors = T)
 
 #TRATAMENTO DE DADOS E LIMPEZA
-#alterando nome das variáveis
-summary(dados)
-dados_alterado <- rename(dados, municipio = nome_munic, data = datahora)
 
-#Entendendo os dados
-  #criar uma nova tabela e aplicar o filtro retirando dados vazias
-  #pegar a média dessa nova tabela em casos e casos_novos
-  #preencher dados faltantes com a média
-summary(dados_alterado$casos)
-summary(dados_alterado$casos_novos)
-hist(dados_alterado$casos_novos)
-hist(dados_alterado$casos)
+#Renomeando variáveis
+dados_alterado <- rename(dados, municipio = nome_munic,
+                         data = datahora,
+                         casos_total = casos,
+                         obitos_total = obitos)
+#Excluindo variáveis que não serão usadas
+dados_alterado <- select(dados_alterado, -c(2:4,15,16,22:26))
+dados_alterado <- filter(dados_alterado, casos_novos != 0)  
 
-#Filtro de dados
-dados_completos <- filter(dados_alterado,dados_alterado$casos != 0)
-dados_completos <- filter(dados_completos,dados_completos$casos_novos != 0)
-dados_completos <- filter(dados_completos,dados_completos$municipio != "Ignorado")
-summary(dados_completos$municipio)
+#Ajuste das variáveis
+glimpse(dados_alterado)
+dados_alterado$data <- as.Date(dados_alterado$data)
+dados_alterado$casos_pc <- as.integer(dados_alterado$casos_pc)
+dados_alterado$casos_mm7d <- as.integer(dados_alterado$casos_mm7d)
+dados_alterado$obitos_pc <- as.integer(dados_alterado$obitos_pc)
+dados_alterado$obitos_mm7d <- as.integer(dados_alterado$obitos_mm7d)
 
-#Filtrando por municipios
+#Análise estatística
 
-#São paulo
-dados_sp <- filter(dados_completos, dados_completos$municipio == "São Paulo")
-hist(dados_sp$casos)
-#exclusão de valores muito pequenos
-dados_sp <- slice(dados_sp,-(1:4))
